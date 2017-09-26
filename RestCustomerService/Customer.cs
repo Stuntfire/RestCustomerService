ed48a2baff92f7af0b964c5bdc2fcba7f7b51098
Søren Roed Schack <soer622t@edu.easj.dk>
2017-09-26 09:14:21 +0200

    Tilføjet en Customer klasse.

---
+++ RestCustomerService/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestCustomerService
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Year { get; set; }
+
+        public Customer(int id, string first, string last, int year)
+        {
+            this.Id = id;
+            this.FirstName = first;
+            this.LastName = last;
+            this.Year = year;
+        }
+
+        public Customer()
+        {
+            
+        }
+    }
+}
